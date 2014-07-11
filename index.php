6b8d06672af6a37a18249989bc94eb73f1fac9bb
Daniel Douat <daniel.douat@aelys-info.fr>
2014-07-11 13:12:10 +0200

    cleaned code

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php
 if (file_exists('captcha.php')) {
-	require_once 'captcha.php';
+    require_once 'captcha.php';
 }
 if (file_exists('blacklist.php')) {
-	require_once 'blacklist.php';
+    require_once 'blacklist.php';
 }
 
 function toVCard($file)
 {
-	if (file_exists($file))
-	{
-	    $data     = file_get_contents($file);
-	    $contacts = preg_split('//', $data);
-	    array_pop($contacts);
-	    if (is_array($contacts)) {
-	    	$vCard = '';
-	    	foreach ($contacts as $key => $value) {
-	    		$element = trim($value);
-	    		preg_match('/^FirstName:(.*)$/m', $element, $firstName);
-	    		preg_match('/^LastName:(.*)$/m', $element, $lastName);
-	    		preg_match('/^JobTitle:(.*)$/m', $element, $fn);
-	    		preg_match('/^MailAddress:(.*)$/m', $element, $firstEmail);
-	    		preg_match('/^InternetAddress:(.*)$/m', $element, $secondEmail);
-	    		preg_match('/^OfficePhoneNumber:(.*)$/m', $element, $officePhone);
-	    		preg_match('/^OfficeFAXPhoneNumber:(.*)$/m', $element, $officeFax);
-	    		preg_match('/^CellPhoneNumber:(.*)$/m', $element, $cellPhone);
-	    		preg_match('/^PhoneNumber:(.*)$/m', $element, $homePhone);
-	    		preg_match('/^HomeFAXPhoneNumber:(.*)$/m', $element, $homeFax);
-	    		preg_match('/^OfficeStreetAddress: (.*)$/m', $element, $officeStreet);
-	    		preg_match('/^OfficeCity:(.*)$/m', $element, $officeCity);
+    if (file_exists($file))
+    {
+        $data     = file_get_contents($file);
+        $contacts = preg_split('//', $data);
+        array_pop($contacts);
+        if (is_array($contacts)) {
+            $vCard = '';
+            foreach ($contacts as $key => $value) {
+                $element = trim($value);
+                preg_match('/^FirstName:(.*)$/m', $element, $firstName);
+                preg_match('/^LastName:(.*)$/m', $element, $lastName);
+                preg_match('/^JobTitle:(.*)$/m', $element, $fn);
+                preg_match('/^MailAddress:(.*)$/m', $element, $firstEmail);
+                preg_match('/^InternetAddress:(.*)$/m', $element, $secondEmail);
+                preg_match('/^OfficePhoneNumber:(.*)$/m', $element, $officePhone);
+                preg_match('/^OfficeFAXPhoneNumber:(.*)$/m', $element, $officeFax);
+                preg_match('/^CellPhoneNumber:(.*)$/m', $element, $cellPhone);
+                preg_match('/^PhoneNumber:(.*)$/m', $element, $homePhone);
+                preg_match('/^HomeFAXPhoneNumber:(.*)$/m', $element, $homeFax);
+                preg_match('/^OfficeStreetAddress: (.*)$/m', $element, $officeStreet);
+                preg_match('/^OfficeCity:(.*)$/m', $element, $officeCity);
                 preg_match('/^OfficeState:(.*)$/m', $element, $officeState);
                 preg_match('/^OfficeZIP:(.*)$/m', $element, $officeZip);
                 preg_match('/^OfficeCountry:(.*)$/m', $element, $officeCountry);
-	    		preg_match('/^StreetAddress:(.*)$/m', $element, $homeStreetAddress);
-	    		preg_match('/^City:(.*)$/m', $element, $homeCity);
+                preg_match('/^StreetAddress:(.*)$/m', $element, $homeStreetAddress);
+                preg_match('/^City:(.*)$/m', $element, $homeCity);
                 preg_match('/^State:(.*)$/m', $element, $homeState);
                 preg_match('/^Zip:(.*)$/m',$element, $homeZip);
                 preg_match('/^country:(.*)$/m', $element, $homeCountry);
@@ -56,133 +56,125 @@ function toVCard($file)
                     $homeCity[1] = $homeCityTemp[2];
                 }
 
-
-
-	    		$vCard .= 'BEGIN:VCARD' . PHP_EOL;
-	    		$vCard .= 'VERSION:3.0' . PHP_EOL;
-	    		$vCard .= 'N:' . trim($lastName[1]) . ';' . trim($firstName[1]) . ';;;' . PHP_EOL;
-	    		$vCard .= 'FN:' . trim($firstName[1]) . ' ' . trim($lastName[1]) . PHP_EOL; // and str_replace('FN:' . PHP_EOL, 'FN:Noreply' . PHP_EOL, $vcard)
-	    		$vCard .= 'EMAIL;TYPE;INTERNET;TYPE=WORK:' . trim($firstEmail[1]) . PHP_EOL;
-	    		$vCard .= 'EMAIL;TYPE;INTERNET;TYPE=HOME:' . trim($secondEmail[1]) . PHP_EOL;
-	    		$vCard .= 'TEL;TYPE=WORK:' . trim($officePhone[1]) . PHP_EOL;
-	    		$vCard .= 'TEL;TYPE=WORKFAX:' . trim($officeFax[1]) . PHP_EOL;
-	    		$vCard .= 'TEL;TYPE=CELL:' . trim($cellPhone[1]) . PHP_EOL;
-	    		$vCard .= 'TEL;TYPE=HOME:' . trim($homePhone[1]) . PHP_EOL;
-	    		$vCard .= 'TEL;TYPE=HOMEFAX:' . trim($homeFax[1]) . PHP_EOL;
-	    		$vCard .= 'ADR;TYPE=WORK:;;' . trim($officeStreet[1]) . ';' . trim($officeCity[1]) . ';' . trim($officeState[1]) . ';' . trim($officeZip[1]) . ';' . trim($officeContry[1]) . PHP_EOL;
-	    		$vCard .= 'ADR;TYPE=HOME:;;' . trim($homeStreet[1]) . ';' . trim($homeCity[1]) . ';' . trim($homeState[1]) . ';' . trim($homeZip[1]) . ';' . trim($homeContry[1]) . PHP_EOL;
-	    		$vCard .= 'END:VCARD' . PHP_EOL;
-	    	}
-	    	return utf8_encode(trim($vCard));
-	    }
-	}
+                $vCard .= 'BEGIN:VCARD' . PHP_EOL;
+                $vCard .= 'VERSION:3.0' . PHP_EOL;
+                $vCard .= 'N:' . trim($lastName[1]) . ';' . trim($firstName[1]) . ';;;' . PHP_EOL;
+                $vCard .= 'FN:' . trim($firstName[1]) . ' ' . trim($lastName[1]) . PHP_EOL; // and str_replace('FN:' . PHP_EOL, 'FN:Noreply' . PHP_EOL, $vcard)
+                $vCard .= 'EMAIL;TYPE;INTERNET;TYPE=WORK:' . trim($firstEmail[1]) . PHP_EOL;
+                $vCard .= 'EMAIL;TYPE;INTERNET;TYPE=HOME:' . trim($secondEmail[1]) . PHP_EOL;
+                $vCard .= 'TEL;TYPE=WORK:' . trim($officePhone[1]) . PHP_EOL;
+                $vCard .= 'TEL;TYPE=WORKFAX:' . trim($officeFax[1]) . PHP_EOL;
+                $vCard .= 'TEL;TYPE=CELL:' . trim($cellPhone[1]) . PHP_EOL;
+                $vCard .= 'TEL;TYPE=HOME:' . trim($homePhone[1]) . PHP_EOL;
+                $vCard .= 'TEL;TYPE=HOMEFAX:' . trim($homeFax[1]) . PHP_EOL;
+                $vCard .= 'ADR;TYPE=WORK:;;' . trim($officeStreet[1]) . ';' . trim($officeCity[1]) . ';' . trim($officeState[1]) . ';' . trim($officeZip[1]) . ';' . trim($officeContry[1]) . PHP_EOL;
+                $vCard .= 'ADR;TYPE=HOME:;;' . trim($homeStreet[1]) . ';' . trim($homeCity[1]) . ';' . trim($homeState[1]) . ';' . trim($homeZip[1]) . ';' . trim($homeContry[1]) . PHP_EOL;
+                $vCard .= 'END:VCARD' . PHP_EOL;
+            }
+            return utf8_encode(trim($vCard));
+        }
+    }
 }
 
 function sendVcardByMail($mailTo, $vCard)
 {
-	if (!empty($mailTo)) {
-            $boundary = md5(rand()); 
-            $boundAtt = md5(rand());
-		    $fromMail = $mailTo; //Expediteur  
-		    $fromName = $GLOBALS['scriptInfo']; 
-		    $subject  = 'Votre carnet d\'adresses';      
-		    //Génération du séparateur   
-		    $header[] = 'From: ' . $fromMail;  
-		    $header[] = 'Reply-to: ' . $fromMail;  
-		    $header[] = 'X-Priority: 1';  
-		    $header[] = 'MIME-Version: 1.0';  
-		    $header[] = 'Content-Type: multipart/mixed; boundary="' . $boundary . '"';  
-		    //$header[] = ' \n';  
-		    $content[] = '--=' . $boundary;  
-		    $content[] = 'Content-Type: text/plain; charset="utf-8"';  
-		    $content[] = 'Content-Transfer-Encoding:8bit';  
-		    //$content[] = '\n';  
-		    $content[] = 'Voici votre carnet d\'adresses disponible en pièces jointe';  
-		    //$content[] = '\n';  
+    if (!empty($mailTo)) {
+            $boundary  = md5(rand()); 
+            $boundAtt  = md5(rand());
+            $fromMail  = $mailTo;
+            $fromName  = $GLOBALS['scriptInfo']; 
+            $subject   = 'Votre carnet d\'adresses';      
+            $header[]  = 'From: ' . $fromMail;  
+            $header[]  = 'Reply-to: ' . $fromMail;  
+            $header[]  = 'X-Priority: 1';  
+            $header[]  = 'MIME-Version: 1.0';  
+            $header[]  = 'Content-Type: multipart/mixed; boundary="' . $boundary . '"';  
+            $content[] = '--=' . $boundary;  
+            $content[] = 'Content-Type: text/plain; charset="utf-8"';  
+            $content[] = 'Content-Transfer-Encoding:8bit';  
+            $content[] = 'Voici votre carnet d\'adresses disponible en pièces jointe';  
             $content[] = '--' . $boundary;
-            //$content[] = '--' . $boundary;
-		    $content[] = 'Content-Transfer-Encoding: base64';  
+            $content[] = 'Content-Transfer-Encoding: base64';  
             $content[] = 'Content-Type: text/vcard; name=vcard.vcf';
             $content[] = 'Content-Disposition: attachment; filename=vcard.vcf;';
-		    //$content[] = '\n';  
-		    $content[] = chunk_split(base64_encode($vCard)); 
-		    $content[] = '--' . $boundary . '--'; 
-		    $content[] = '--' . $boundary . '--'; 
-			return mail($mailTo, $subject, implode("\r\n", $content), implode("\r\n", $header));
-	}
+            $content[] = chunk_split(base64_encode($vCard)); 
+            $content[] = '--' . $boundary . '--'; 
+            $content[] = '--' . $boundary . '--'; 
+            return mail($mailTo, $subject, implode("\r\n", $content), implode("\r\n", $header));
+    }
 }
 
 function blackList($action)
 {
-	$key  = $_SERVER['REMOTE_ADDR'];
-	$time = microtime(true);
-	if (!empty($action)) {
-		if ($action === 'add') {
-			$GLOBALS['blackList'][$key] = $time;
-			$result = $time;
-		}
-		elseif ($action === 'del') {
-			unset($GLOBALS['blackList'][$key]);
-			$result = false;
-		}
-		if (is_writable($_SERVER['DOCUMENT_ROOT'])) {
-			$content  = '<?php' . PHP_EOL;
-			$content .= '$blackList = ' . var_export($GLOBALS['blackList'], true) . ';' . PHP_EOL;
-			$content .= '?>'; 
-			file_put_contents('blacklist.php', $content);
-		}
-	}
-	elseif (is_array($GLOBALS['blackList'])) {
-		if (array_key_exists($key, $GLOBALS['blackList'])) {
-			$result = $GLOBALS['blackList'][$key];
-		}
-		else {
-			$result = false;
-		}
-	}
-	else {
-		$result = false;
-	}
-	return $result;
+    $key  = $_SERVER['REMOTE_ADDR'];
+    $time = microtime(true);
+    if (!empty($action)) {
+        if ($action === 'add') {
+            $GLOBALS['blackList'][$key] = $time;
+            $result = $time;
+        }
+        elseif ($action === 'del') {
+            unset($GLOBALS['blackList'][$key]);
+            $result = false;
+        }
+        if (is_writable($_SERVER['DOCUMENT_ROOT'])) {
+            $content  = '<?php' . PHP_EOL;
+            $content .= '$blackList = ' . var_export($GLOBALS['blackList'], true) . ';' . PHP_EOL;
+            $content .= '?>'; 
+            file_put_contents('blacklist.php', $content);
+        }
+    }
+    elseif (is_array($GLOBALS['blackList'])) {
+        if (array_key_exists($key, $GLOBALS['blackList'])) {
+            $result = $GLOBALS['blackList'][$key];
+        }
+        else {
+            $result = false;
+        }
+    }
+    else {
+        $result = false;
+    }
+    return $result;
 }
 
 function setCaptcha()
 {
-	$nbCapchas           = count($GLOBALS['captchas']);
-	$randomKey           = mt_rand(0, $nbCapchas - 1);
-	$token               = md5(mt_rand());
-	$captcha             = $GLOBALS['captchas'][$randomKey];
-	$GLOBALS['captcha']  = '<input type="text" name="hgiozjdjgfg" id="hgiozjdjgfg" value="" placeholder="' . $captcha['question'] . '">' . PHP_EOL;
-	$GLOBALS['captcha'] .= '<input type="hidden" name="pqdhnsljkqzbhxh" value="' . $token . '">' . PHP_EOL;
-	$_SESSION['cToken']  = $token;
-	$_SESSION['cKey']    = $randomKey;
+    $nbCapchas           = count($GLOBALS['captchas']);
+    $randomKey           = mt_rand(0, $nbCapchas - 1);
+    $token               = md5(mt_rand());
+    $captcha             = $GLOBALS['captchas'][$randomKey];
+    $GLOBALS['captcha']  = '<input type="text" name="hgiozjdjgfg" id="hgiozjdjgfg" value="" placeholder="' . $captcha['question'] . '">' . PHP_EOL;
+    $GLOBALS['captcha'] .= '<input type="hidden" name="pqdhnsljkqzbhxh" value="' . $token . '">' . PHP_EOL;
+    $_SESSION['cToken']  = $token;
+    $_SESSION['cKey']    = $randomKey;
 }
 
 function controlCaptcha($answer, $token)
 {
-	if ($_SESSION['cToken'] === $token && !empty($answer)) {
-		$key = $_SESSION['cKey'];
-		if (!array_key_exists($key, $GLOBALS['captchas']) || $GLOBALS['captchas'][$key]['answer'] !== $answer) {
-			unset($_GET, $_POST, $_FILES);
-			blackList('add');
-			initCaptcha();
-		}
-	}
-	else {
-		unset($_GET, $_POST, $_FILES);
-		blackList('add');
-		initCaptcha();
-	}
+    if ($_SESSION['cToken'] === $token && !empty($answer)) {
+        $key = $_SESSION['cKey'];
+        if (!array_key_exists($key, $GLOBALS['captchas']) || $GLOBALS['captchas'][$key]['answer'] !== $answer) {
+            unset($_GET, $_POST, $_FILES);
+            blackList('add');
+            initCaptcha();
+        }
+    }
+    else {
+        unset($_GET, $_POST, $_FILES);
+        blackList('add');
+        initCaptcha();
+    }
 }
 
 function initCaptcha()
 {
-	if (!empty($_POST) && is_array($GLOBALS['captchas'])) {
-		controlCaptcha(addslashes($_POST['hgiozjdjgfg']), addslashes($_POST['pqdhnsljkqzbhxh']));
-	}
-	elseif (is_array($GLOBALS['captchas'])) {
-		setCaptcha();
-	}
+    if (!empty($_POST) && is_array($GLOBALS['captchas'])) {
+        controlCaptcha(addslashes($_POST['hgiozjdjgfg']), addslashes($_POST['pqdhnsljkqzbhxh']));
+    }
+    elseif (is_array($GLOBALS['captchas'])) {
+        setCaptcha();
+    }
 }
 
 (blackList() !== false) ? exit('Ah ah ah, you didn\'t say the magic word !') : '';
@@ -193,25 +185,22 @@ $_SESSION['startTime'] = microtime(true);
 initCaptcha();
 
 if (!empty($_POST) && is_array($_FILES)) {
-	if (is_uploaded_file($_FILES['file']['tmp_name'])) {
-		$vCard = toVCard($_FILES['file']['tmp_name']);
-		//header('Content-Type: text/x-vcard; charset=utf-8');
-		//echo $vCard;
-		//$temp = tmpfile();
-		//$attachedFile = file_put_contents($temp, $vCard);
-		$send = sendVcardByMail($_POST['email'], $vCard);
-		print_r($send);
+    if (is_uploaded_file($_FILES['file']['tmp_name'])) {
+        $vCard = toVCard($_FILES['file']['tmp_name']);
+        $send = sendVcardByMail($_POST['email'], $vCard);
+        print_r($send);
 
-	}
-	exit();
+    }
+    exit();
 }
 ?>
+
 <html>
 <meta charset="utf-8">
 <head>
-	<title>Lotus contacts to vcard</title>
-	<style type="text/css">
-	body {
+    <title>Lotus contacts to vcard</title>
+    <style type="text/css">
+    body {
         margin-top: 5%;
         font-family: Corbel, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, "Verdana Ref", sans-serif;
         font-size: 0.8em;
@@ -241,7 +230,7 @@ if (!empty($_POST) && is_array($_FILES)) {
         padding: 2px;
     }
     input[type=file] {
-    	height: auto;
+        height: auto;
     }
     input[type=text], input[type=password], input[type=email], input[type=file] {
         width: 100%;
@@ -288,21 +277,21 @@ if (!empty($_POST) && is_array($_FILES)) {
     #credits a:hover, #credits a:focus{
         color: #222;
     }
-	
-	</style>
+    
+    </style>
 </head>
 <body>
-	<div>
-		<form enctype="multipart/form-data" name="upload contacts" method="post">
-			<input type="file" name="file">
-			<input type="email" name="email" value="" placeholder="Indiquez votre adresse email">
-			<?php if (!empty($GLOBALS['captcha'])) {
-				echo $GLOBALS['captcha'];
-			} ?>
-			<input type="submit" name="submit" value="Générer">
-		</form>
-	</div>
-	<span class="error"></span>
+    <div>
+        <form enctype="multipart/form-data" name="upload contacts" method="post">
+            <input type="file" name="file">
+            <input type="email" name="email" value="" placeholder="Indiquez votre adresse email">
+            <?php if (!empty($GLOBALS['captcha'])) {
+                echo $GLOBALS['captcha'];
+            } ?>
+            <input type="submit" name="submit" value="Générer">
+        </form>
+    </div>
+    <span class="error"></span>
     <span id="credits"><a href="https://github.com/Roultabie/lotus5ContactsToVCard" target="_blank">lotus5ContactsToVCard on GitHub</a></span>
 </body>
 </html>
